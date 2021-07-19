faffd5b32c096bbe60a380540900f3155ece9095
Sultan Baimukhanov <portret008@gmail.com>
2021-07-19 21:59:08 +0600

    host

--- src/redux/Developer-selector.tsx
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "reselect"
-import { AppStateType } from "./redux-store";
-
-const getDevelopersSelector = (state: AppStateType) =>{
-    return state.developersPage.developers
-}
-export const getDevelopersReselect = createSelector(getDevelopersSelector, (developers) => {
-    return developers;
-})
